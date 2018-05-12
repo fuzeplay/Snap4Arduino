19ae4ff37d20dd095be03a08d9d73ad0682a54bc
Jordan Rejaud <jrejaud@gmail.com>
2018-05-11 17:09:55 -0700

    Added logic directly in extension js file

--- src/platforms/web/chromium/crx/bundle.js
+++ src/platforms/web/chromium/crx/bundle.js
@@ -95,6 +95,14 @@ Dispatcher.prototype.getArduinoBoardParam = function (boardId, name) {
     return boardNameTemp;
 };
 
+//Adds extra functionality to the chrome extension in order to handle
+//Fuze specific functionality
+
+Dispatcher.prototype.setLEDColor = function (boardId, ledPin, colorCode) {
+  Boards[boardId].setLEDColor(ledPin, colorCode);
+}
+
+
 
 },{"firmata":16}],2:[function(require,module,exports){
 'use strict'
@@ -8383,6 +8391,23 @@ Board.prototype.digitalWrite = function(pin, value) {
 Board.prototype.digitalRead = function(pin, callback) {
   this.reportDigitalPin(pin, 1);
   this.addListener("digital-read-" + pin, callback);
+};
+
+
+//Used to ID this message as an LED_STRIP message
+const LED_STRIP = 0x7C;
+/**
+* Asks the zubi flyer to send an LED color
+* @param {number} ledPin The ledPin that you want to change. Can be set to ALL_LEDS = 0x7F to set all LEDs
+* @param {number} colorCode The colorCode that you want to set the LED(s) to. Can be set to CLEAR_LED = 0x7F to clear the LED(s)
+*/
+Board.prototype.setLEDColor = function(ledPin, colorCode) {
+  writeToTransport(this, [
+    START_SYSEX,
+    LED_STRIP,
+    ledPin,
+    colorCode,
+    END_SYSEX]);
 };
 
 /**
